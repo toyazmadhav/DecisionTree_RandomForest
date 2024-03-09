b12746d7b92ad871309bd581f32ee49457c724f8
madhav <sai.madhav@bandhoo.com>
2024-03-09 17:40:38 +0530

    added steamlit and voting classifer

--- speech_recognition.py
+++ speech_recognition.py
@@ -16,13 +16,16 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import VotingClassifier, BaggingClassifier, RandomForestClassifier
 from sklearn.inspection import permutation_importance
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report
 
 from matplotlib import pyplot as plt
 
 import librosa.display
 import soundfile
 import IPython.display as ipd
+
+import warnings
+warnings.filterwarnings("ignore")
 
 def download_data():
   url = 'https://cdn.iisc.talentsprint.com/CDS/MiniProjects/Ravdess_Tess.zip'
@@ -48,11 +51,12 @@ def display_sample_audio_waveform(ravdess_wav_files):
     sample_audio_path = ravdess_wav_files[4]
   # librosa is used for analyzing and extracting features of an audio signal
     data, sampling_rate = librosa.load(sample_audio_path)
-    plt.figure(figsize=(15, 5))
+    fig = plt.figure(figsize=(15, 5))
     # plt.title('Sample Audio Waveform')
   # librosa.display.waveshow is used to plot waveform of amplitude vs time
     _ = librosa.display.waveshow(data, sr=sampling_rate)
     plt.savefig('plots/sample_audio_waveform.png')
+    st.pyplot(fig)
 
 def extract_feature(file_name):
     # load audio
@@ -100,7 +104,6 @@ def extract_features_and_lables(ravdess_wav_files, ravdess_mapping, ravdess_emt_
 def main():
   #download and extract zip file
   file_name = download_data()
-  print(file_name)
   extracted_folder = 'Ravdess_Tess'
   if(not os.path.exists(extracted_folder)):
     with zipfile.ZipFile(file_name, 'r') as zip_ref:
@@ -114,10 +117,11 @@ def main():
   # calm is not available in training data set so can be removed while training
   ravdess_emotions_count = all_emotions_in_dataset(ravdess_wav_files, ravdess_mapping, ravdess_emt_logic)
   print(ravdess_emotions_count)
-  plt.figure(figsize=(10,10))
+  fig = plt.figure(figsize=(10,10))
   plt.title('Ravdess countplot')
   plt.plot(ravdess_emotions_count.keys(), ravdess_emotions_count.values())
   plt.savefig('plots/ravdess_class_counts.png')
+  st.pyplot(fig)
 
   display_sample_audio_waveform(ravdess_wav_files)
   # ipd.Audio( ravdess_wav_files[4])
@@ -128,10 +132,11 @@ def main():
   tess_emt_logic = lambda x: x.split("/")[-1].split(".")[0].split("_")[-1].lower()
   tess_emotions_count = all_emotions_in_dataset(tess_wav_files, tess_mapping, tess_emt_logic)
   print(tess_emotions_count)
+  fig = plt.figure(figsize=(10,10))
   plt.title('Tess countplot')
-  plt.figure(figsize=(10,10))
   plt.plot(tess_emotions_count.keys(), tess_emotions_count.values())
   plt.savefig('plots/tess_class_counts.png')
+  st.pyplot(fig)
   
   df_features, df_labels = extract_features_and_lables(ravdess_wav_files, ravdess_mapping,\
                                   ravdess_emt_logic, tess_wav_files, tess_mapping, tess_emt_logic)
@@ -140,17 +145,49 @@ def main():
   y = df_labels.copy()
 
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, stratify=y, random_state = 14)
-  rf_clf = RandomForestClassifier(n_estimators=500, random_state=123)
-  rf_clf = rf_clf.fit(X_train, y_train)
 
+
+  # Random Forest classifier
+  print("---------------------------------")
+  st.write("# Random Forest Classifier")
+  print("-------------------")
+  rf_clf = RandomForestClassifier(n_estimators = 500, random_state = 123)
+  rf_clf = rf_clf.fit(X_train, y_train)
   y_pred = rf_clf.predict(X_test)
   print(rf_clf.__class__.__name__, accuracy_score(y_test, y_pred))
-
   y_pred_train = rf_clf.predict(X_train)
   print(rf_clf.__class__.__name__, accuracy_score(y_train, y_pred_train))
+  st.write("## Important features")
+  imp_features = np.array([[idx, score] for (idx ,score) in enumerate(rf_clf.feature_importances_) if score > 0.009])
+  st.write(imp_features)
 
-  imp_features = [(idx, score) for (idx ,score) in enumerate(rf_clf.feature_importances_) if score > 0.009]
-  print(imp_features)
+  # st.write("# Random Forest Classifier with important features only")
+  # X_train_imp_features = X_train[:, imp_features[:, 1]]
 
-  st.pyplot()
+  # Voting classifiers
+  log_clf = LogisticRegression(random_state=42)
+  svm_clf = SVC(probability= True, random_state = 42)
+  dt_clf = DecisionTreeClassifier(max_depth = 10, random_state = 42)
+  rf_clf = RandomForestClassifier(n_estimators = 500, random_state = 42)
+  classifiers = [('lr', log_clf), ('svc', svm_clf), ('dec_tree', dt_clf), ('rf', rf_clf)]
+  voting_clf = VotingClassifier(estimators = classifiers, voting = 'soft')
+
+  for clf in (log_clf, svm_clf, dt_clf, rf_clf, voting_clf):
+    print('------------------------------')
+    st.write("# " + clf.__class__.__name__)
+    print('----------------')
+    clf.fit(X_train, y_train)
+    st.write('## Test data Accuracy')
+    print('----------------')
+    y_pred = clf.predict(X_test)
+    st.write(accuracy_score(y_test, y_pred))
+    st.dataframe(pd.DataFrame(classification_report(y_test, y_pred, output_dict=True)))
+
+    print('----------------')
+    st.write('## Training data Accuracy')
+    print('----------------')
+    y_pred_train = clf.predict(X_train)
+    st.write(accuracy_score(y_train, y_pred_train))
+    st.dataframe(pd.DataFrame(classification_report(y_train, y_pred_train, output_dict=True)))
+    
 main()
